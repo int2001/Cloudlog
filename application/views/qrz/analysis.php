37dd622a7504e40dd9a7c43738d210e4f5b10da4
int2001 <joerg@dj7nt.de>
2023-12-08 04:42:21 +0000

    Added Analysis

--- application/views/qrz/analysis.php
+++ application/views/qrz/analysis.php
@@ -1,13 +1,13 @@
 <div class="container">
 <h2><?php echo $page_title; ?></h2>
 
-<?php 
-if (isset ($qrz_table_headers)) {
-	echo $qrz_table_headers;
+<?php
+if (isset ($table_headers)) {
+        echo $table_headers;
 } else {
-	echo 'No data imported. please check selected date. Must be in the past!';
+        echo 'No data imported. please check selected date. Must be in the past!';
 }
 ?>
-<?php if (isset ($qrz_table)) {echo $qrz_table;} ?>
+<?php if (isset ($table)) {echo $table;} ?>
 
 </div>
